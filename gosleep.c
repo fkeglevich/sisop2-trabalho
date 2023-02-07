242373c19d516cad48c83ae42556bb1013e861b7
Flávio Keglevich <fkeglevich@gmail.com>
2023-02-07 15:58:23 -0300

    Fixed gosleep script :3

---
+++ gosleep.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int main(){
+	system("systemctl suspend");
+	return 0;
+}
--- gosleep.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstring>
-#include <stdlib.h>
-int main(){
-	string str = "systemctl hibernate";
-	const char *command = str.c_str();
-	system(command);
-	return 0;
-}
