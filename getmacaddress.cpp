a63875f4c87e35581c20d8031dc55712439985b5
Flávio Keglevich <fkeglevich@gmail.com>
2023-02-07 21:21:14 -0300

    Added comment for getting MAC address of WiFi card

--- getmacaddress.cpp
+++ getmacaddress.cpp
@@ -6,6 +6,7 @@ int main ()
 {
     //1st way to get the MAC address
     string str1 = "cat /sys/class/net/eth0/address";
+    //use "cat /sys/class/net/wlp*/address" for getting wifi mac address
     const char *command1 = str1.c_str();
     //2nd way to get MAC address
     string str2 = "ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' "; 
